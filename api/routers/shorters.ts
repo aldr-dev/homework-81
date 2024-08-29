41f84b25c88d0edb065c021484706156a2fc47b0
Gordus Alexander <aldrmiller@gmail.com>
2024-08-30 00:58:56 +0600

    реализовал get роутер для получения сокращенной ссылки и редиректа на оригиналный url.

--- api/routers/shorters.ts
+++ api/routers/shorters.ts
@@ -1,7 +1,21 @@
 import express from 'express';
+import Shorter from '../models/Shorter';
 
 const shortersRouter = express.Router();
 
+shortersRouter.get('/:shortUrl', async (req, res, next) => {
+  try {
+    const shortUrl = req.params.shortUrl;
+    const url = await Shorter.findOne({shortUrl});
 
+    if (!url) {
+      return res.status(404).send({error: 'Link not found'});
+    }
+
+    res.status(301).redirect(url.originalUrl);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default shortersRouter;
